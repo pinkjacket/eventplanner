5274f93abed862c3eda80e4ab30e429fcea35f65
James Donlan <jacketlizard@live.com>
2018-01-05 12:51:27 -0800

    first test passes

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class EventTest {
     @Test
     public void getGuests_getOnePerson_1() {
-    Event testEvent = new Event(0);
+    Event testEvent = new Event(1);
     assertEquals(1,testEvent.getGuests());
     }
 
