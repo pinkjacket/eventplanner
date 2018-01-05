8a285c6496ad1e78ae3db3b2f016937229fc5a44
James Donlan <jacketlizard@live.com>
2018-01-05 13:52:02 -0800

    entertainment level implemented, failing test

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,11 +2,13 @@ public class Event {
     private int people;
     private int foodType;
     private int drinkType;
+    private int showType;
 
-    public Event(int numGuests, int foodLevel, int drinkLevel) {
+    public Event(int numGuests, int foodLevel, int drinkLevel, int showLevel) {
         people = numGuests;
         foodType = foodLevel;
         drinkType = drinkLevel;
+        showType = showLevel;
     }
 
     public int getGuests() {
@@ -14,7 +16,7 @@ public class Event {
     }
 
     public int getDetails() {
-        int cost = people * (foodType + drinkType + 10);
+        int cost = people * (foodType + drinkType + showType + 10);
         return cost;
     }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -4,26 +4,32 @@ import static org.junit.Assert.*;
 public class EventTest {
     @Test
     public void getGuests_getOnePerson() {
-    Event testEvent = new Event(1, 0, 0);
+    Event testEvent = new Event(1, 0, 0, 0);
     assertEquals(1,testEvent.getGuests());
     }
 
     @Test
     public void getGuests_getMorePeople() {
-        Event testEvent = new Event(50, 0, 0);
+        Event testEvent = new Event(50, 0, 0, 0);
         assertEquals(50,testEvent.getGuests());
     }
 
     @Test
     public void getDetails_addFood() {
-        Event testEvent = new Event (50, 5, 0);
+        Event testEvent = new Event (50, 5, 0, 0);
         assertEquals (750, testEvent.getDetails());
     }
 
     @Test
     public void getDetails_addDrink() {
-        Event testEvent = new Event (50, 5, 3);
-        assertEquals (750, testEvent.getDetails());
+        Event testEvent = new Event (50, 5, 3, 0);
+        assertEquals (900, testEvent.getDetails());
+    }
+
+    @Test
+    public void getDetails_addShow() {
+        Event testEvent = new Event (50, 5, 3, 4);
+        assertEquals (1150, testEvent.getDetails());
     }
 
 }
